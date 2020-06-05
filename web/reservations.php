7bed1b3f16d0b68443ee1d050b6a0f61f7d0997b
Shannon Birch <helicopterdasher@gmail.com>
2020-06-05 10:16:07 -0400

    Update reservations.php

--- web/reservations.php
+++ web/reservations.php
@@ -14,7 +14,6 @@
 		<?php require 'php_partials/navbar.php';?>
 		<?php include 'scripts/viewAllReservations.php';?>
 		
-		<!--TODO: WRITE CODE YOU LAZY PIECE OF SHIT -->
 		
 		<?php include 'php_partials/footer.php';?>
 	</body>
